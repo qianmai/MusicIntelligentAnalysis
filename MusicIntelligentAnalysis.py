51f05349c8560fdc9f5f1564a48a5b31bf961cf2
qianmai <chennymai@gmail.com>
2016-12-04 18:11:35 -0700

    Create

---
+++ MusicIntelligentAnalysis.py
@@ -0,0 +1,32 @@
+from scipy.io import wavfile
+import wave
+import numpy as np
+import Output
+
+fileName1 = 'music/scout_1000.wav'
+fileName2 = 'music/music_test.wav'
+
+def main():
+    #Signal
+    wavFile = wave.open(fileName2, 'r')
+    signal = wavFile.readframes(-1)
+    timeDomain = np.fromstring(signal, 'Int16')
+    
+    
+
+    #Output
+    Output.Plot_Output(timeDomain)
+
+main()
+
+##Fourier
+#fs, data = wavfile.read(fileName1) # load the data
+
+#channel_1 = data.T[0] # this is a two channel soundtrack
+
+#b=[]
+#for ele in range(channel_1):
+#    b.append((ele/2**8.)*2-1) # this is 8-bit track, b is now normalized on [-1,1)
+#c = fft(b) # calculate fourier transform (complex numbers list)
+#d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+
---
+++ Output.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from scipy.io import wavfile
+import numpy as np
+import wave
+import sys
+
+def Plot_Output(timeDomain):
+    #Signal
+    plt.subplot(4, 1, 1)
+    plt.title('Music Analysis', fontsize=40)
+    plt.plot(timeDomain)
+
+    #Channel 1
+    plt.subplot(4, 1, 2)
+    plt.plot()
+
+    #Channel 2
+    plt.subplot(4, 1, 3)
+    plt.plot()
+
+    #Fourier
+    plt.subplot(4, 1, 4)
+    plt.plot()
+
+    plt.show()
