61a17eb55e4dc68537ce0ab53dc00c6ce55a6fc4
qianmai <chennymai@gmail.com>
2016-12-09 00:27:21 -0700

    Enable separate segment

--- src/MusicIntelligentAnalysis.py
+++ src/MusicIntelligentAnalysis.py
@@ -5,10 +5,11 @@ import Output
 import Segmentation
 import FourierTransform as ft
 
-fileName1 = '../music/scout_1000.wav'
-fileName2 = '../music/music_test.wav'
-fileName3 = '../music/happy_piano.wav'
-fileName4 = '../music/guitar.wav'
+#fileName1 = '../music/scout_1000.wav'
+fileName1 = 'music/scout_1000.wav'
+fileName2 = 'music/music_test.wav'
+fileName3 = 'music/happy_piano.wav'
+fileName4 = 'music/guitar.wav'
 
 def main():
     #Signal
@@ -16,14 +17,23 @@ def main():
     signal = wavFile.readframes(-1)
     timeDomain = np.fromstring(signal, 'Int16')
     
+    #Get Key Time List
+    timePointList = Segmentation.get_segment_pairs(timeDomain, 1000, 800)
+    test = len(timePointList)
+    head, tail = Segmentation.split_pair_from_list_by_index(timePointList, 0)
+    head2, tail2 = Segmentation.split_pair_from_list_by_index(timePointList, 1)
+
     # Added by Kun Li.
-    timeDomain = Segmentation.make_segment(timeDomain, 57000, 63000)
+    segmentTimeDomain = Segmentation.make_segment(timeDomain, head, tail)
+    td = Segmentation.make_segment(timeDomain, head2, tail2)
     
     #Frequency Domain
-    frequencyDomain = ft.Transform(timeDomain, 600)
+    frequencyDomain = ft.Transform(segmentTimeDomain, 600)
+    fd = ft.Transform(td, 600)
 
     #Output
-    Output.Plot_Output(timeDomain, frequencyDomain)
+    Output.Plot_Output(timeDomain, segmentTimeDomain, frequencyDomain, td, fd)
+    #Output.Plot_Output(timeDomain, segmentTimeDomain, frequencyDomain, None, None)
     
     
 
--- src/Output.py
+++ src/Output.py
@@ -5,19 +5,27 @@ import numpy as np
 import wave
 import sys
 
-def Plot_Output(timeDomain, frequencyDomain):
-    #Signal
-    plt.subplot(4, 1, 1)
+def Plot_Output(timeDomain, segmentTimeDomain, frequencyDomain, backup_1, backup_2):
+    #Entire Music
+    plt.subplot(5, 1, 1)
     plt.title('Music Analysis', fontsize=40)
     plt.plot(timeDomain)
 
+    #Signal
+    plt.subplot(5, 1, 2)
+    plt.plot(segmentTimeDomain)
+
     #Channel 1
-    plt.subplot(4, 1, 2)
+    plt.subplot(5, 1, 3)
     plt.plot(frequencyDomain)
 
     #specgram
-    plt.subplot(4, 1, 3)
-    plt.plot()
+    plt.subplot(5, 1, 4)
+    if(backup_1 is None):
+        plt.plot()
+    else:
+        plt.plot(backup_1)
+
     #Pxx, freqs, bins, im = plt.specgram(
     #    timeDomain, 
     #    NFFT=timeDomain, 
@@ -26,9 +34,12 @@ def Plot_Output(timeDomain, frequencyDomain):
     #    cmap=plt.cm.gist_heat
     #)
 
-    #Fourier
-    plt.subplot(4, 1, 4)
-    plt.plot()
+    #Backup
+    plt.subplot(5, 1, 5)
+    if(backup_2 is None):
+        plt.plot()
+    else:
+        plt.plot(backup_2)
     
     ##Text
     #plt.text(2, 4,"A test string", fontsize=50)
